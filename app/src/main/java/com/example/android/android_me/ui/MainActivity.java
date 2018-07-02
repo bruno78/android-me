5eb25eb476d436b5c78ab1245f2ce0b3ab39881c
bruno78 <devbruno78@gmail.com>
2018-07-01 22:04:23 -0400

    Complete 04 Exercise

---
+++ app/src/main/java/com/example/android/android_me/ui/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.android.android_me.ui;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.android.android_me.R;
+
+import java.util.List;
+
+/**
+ * Created by brunogtavares on 7/1/18.
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
